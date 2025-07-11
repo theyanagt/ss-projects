841f39516a42a68ee071fc722761d8c6b5babd96
theyanagt <97217373+theyanagt@users.noreply.github.com>
2025-07-11 15:10:11 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  server: {
+    host: true,
+    port: process.env.PORT ? Number(process.env.PORT) : 4321,
+  },
 });
